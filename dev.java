05ec4f08fd9585539109e91cd9c60f24da173274
gcht <gcht@163.com>
2018-08-04 14:45:52 +0800

    Update dev.java

--- dev.java
+++ dev.java
@@ -1,22 +1,8 @@
 package com.yyuap.mkb.test;
-// this is master 2018-08-04 10:10
 
-// this is master 2018-08-04 10:10
 
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
+//update by master
 
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-// this is master 2018-08-04 10:10
-
-//update by zhoulei and gct 
-//update by zhoulei and gct 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
